a424c9f5f9015f22bc9d6176f3cd7d3708627513
Marcos Valério <marcosvaleriox12@hotmail.com>
2026-04-16 14:18:16 -0300

    Add 404 page

---
+++ app/Views/404.view.php
@@ -0,0 +1,11 @@
+<?php 
+
+include '../app/Views/layout/header.php';
+
+include '../app/Views/layout/nav.php';
+
+?>
+
+<h1>Página não encontrada</h1>
+
+<?php include '../app/Views/layout/footer.php'; ?>
