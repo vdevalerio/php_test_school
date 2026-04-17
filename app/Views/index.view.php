cfa4a2a1a8046e0443f8e9f4f378b8819b7b1d2b
Marcos Valério <marcosvaleriox12@hotmail.com>
2026-04-16 23:17:11 -0300

    Redirect home to turma index

--- app/Views/index.view.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include '../app/Views/layout/header.php';
-include '../app/Views/layout/nav.php';
-
-$heading = 'Página Inicial';
-include '../app/Views/layout/banner.php';
-
-include '../app/Views/layout/footer.php';
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 $router = new App\Core\Router();
 
+$router->get('/', 'TurmaController@index');
+
 $router->get('/turmas', 'TurmaController@index');
 $router->get('/turmas/create', 'TurmaController@create');
 $router->post('/turmas', 'TurmaController@store');
