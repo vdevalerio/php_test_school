a787c92d84ebf7574951a19e8449878525b90a66
Marcos Valério <marcosvaleriox12@hotmail.com>
2026-04-18 06:20:07 -0300

    Refactor: put turmas show on the standard model

--- app/Views/turmas/show.php
+++ app/Views/turmas/show.php
@@ -1,32 +1,50 @@
 <?php
 
-include '../app/Views/layout/header.php';
-include '../app/Views/layout/nav.php';
+use App\Models\Turma;
+
+include __DIR__ . '/../layout/header.php';
+include __DIR__ . '/../layout/nav.php';
+
+/**
+ * @var Turma $turma
+ */
 
 $alunos = $turma->alunos();
+$title = "#{$turma->id} - {$turma->nome} - {$turma->ano}";
 ?>
 
-<h1>#<?= $turma->id ?> - <?= $turma->nome ?> - <?= $turma->ano ?></h1>
-
-<?php component('modal-trigger', [
-    'id' => 'criarAluno',
-    'label' => 'Criar Aluno',
-    'variant' => 'primary',
-    'fetchUrl' => '/alunos/create',
-]) ?>
+<h1><?= $title ?></h1>
 
 <?php
-$rows = array_map(fn($aluno) => [
-    'cells' => [$aluno['id'], $aluno['nome'], $aluno['email'], $aluno['criado_em']],
-    'actions' => [
-        'showUrl' => '/alunos/' . $aluno['id'],
-        'editId' => 'editarAluno-' . $aluno['id'],
-        'editFetchUrl' => '/alunos/' . $aluno['id'] . '/edit',
-        'deleteUrl' => '/alunos/' . $aluno['id'],
-    ],
-], $alunos);
+component('modal-trigger', [
+    'id'       => 'criarAluno',
+    'label'    => 'Criar Aluno',
+    'variant'  => 'primary',
+    'fetchUrl' => '/alunos/create',
+]);
+
+$rows = array_map(function($aluno) {
+    $baseUrl = '/alunos/' . $aluno['id'];
+
+    return [
+        'cells' => [
+            $aluno['id'],
+            $aluno['nome'],
+            $aluno['email'],
+            $aluno['criado_em']
+        ],
+        'actions' => [
+            'showUrl'      => $baseUrl,
+            'editId'       => 'editarAluno-' . $aluno['id'],
+            'editFetchUrl' => $baseUrl . '/edit',
+            'deleteUrl'    => $baseUrl,
+        ]];
+}, $alunos);
+
+component('table', [
+    'columns' => ['#', 'Nome', 'E-mail', 'Criado em'],
+    'rows' => $rows
+]);
 ?>
 
-<?php component('table', ['columns' => ['#', 'Nome', 'E-mail', 'Criado em'], 'rows' => $rows]) ?>
-
-<?php include '../app/Views/layout/footer.php'; ?>
+<?php include __DIR__ . '/../layout/footer.php'; ?>
