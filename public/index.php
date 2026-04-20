d510acdcf7ae21fb2c0c447a318d2794c7664e84
Marcos Valério <marcosvaleriox12@hotmail.com>
2026-04-20 00:57:14 -0300

    Add callback to catch uncaught exceptions

--- public/index.php
+++ public/index.php
@@ -1,9 +1,45 @@
 <?php
 
+use App\Core\Response;
+use App\Exceptions\DatabaseException;
+use App\Exceptions\ValidationException;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../app/helpers.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
+
+set_exception_handler(function (\Throwable $e) {
+    if ($e instanceof DatabaseException) {
+        http_response_code(500);
+        Response::view(
+            'errors/500',
+            ['message' => $e->getMessage()]
+        )->send();
+    }
+
+    if ($e instanceof ValidationException) {
+        http_response_code(422);
+        Response::view(
+            'errors/500',
+            ['message' => implode(' ', $e->errors())]
+        )->send();
+    }
+
+    error_log(sprintf(
+        '[%s] %s in %s:%d',
+        get_class($e),
+        $e->getMessage(),
+        $e->getFile(),
+        $e->getLine()
+    ));
+
+    http_response_code(500);
+    Response::view(
+        'errors/500',
+        ['message' => 'Erro inesperado. Tente novamente mais tarde.']
+    )->send();
+});
 
 require_once __DIR__ . '/../routes/web.php';
